7d3c1edf06d9df4bf48b41915ec46a2acf17402b
Zaynutdinov-Zufarbek <zufarbekz@bk.ru>
2020-11-12 12:15:27 +0300

    Start Spring Boot module

---
+++ src/main/java/com/example/MyBookShopApp/data/BookService.java
@@ -0,0 +1,33 @@
+package com.example.MyBookShopApp.data;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class BookService {
+
+    private JdbcTemplate jdbc;
+
+    @Autowired
+    public BookService(JdbcTemplate jdbc) {
+        this.jdbc = jdbc;
+    }
+
+    public List<Book> getBooks() {
+        List<Book> books = jdbc.query("SELECT * FROM books", (ResultSet rs, int rowNum) ->{
+            Book book = new Book();
+            book.setId(rs.getInt("id"));
+            book.setAuthor(rs.getString("author"));
+            book.setTitle(rs.getString("title"));
+            book.setPriceOld(rs.getString("priceOld"));
+            book.setPrice(rs.getString("price"));
+            return book;
+        });
+        return new ArrayList<>(books);
+    }
+}
