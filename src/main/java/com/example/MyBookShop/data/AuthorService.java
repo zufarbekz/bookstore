392f474de06853e6b435a2aeff308502211c336e
Zaynutdinov-Zufarbek <zufarbekz@bk.ru>
2020-12-12 19:27:54 +0300

    Finish HW from module-5

--- src/main/java/com/example/MyBookShop/data/AuthorService.java
+++ src/main/java/com/example/MyBookShop/data/AuthorService.java
@@ -11,22 +11,15 @@ import java.util.stream.Collectors;
 
 @Service
 public class AuthorService {
-    private final JdbcTemplate jdbc;
+    private AuthorRepository authorRepository;
 
     @Autowired
-    public AuthorService(JdbcTemplate jdbc) {
-        this.jdbc = jdbc;
+    public AuthorService(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
     }
 
     public Map<String, List<Author>> getAuthorsMap() {
-        List<Author> authors = jdbc.query("SELECT * FROM authors",
-                (ResultSet rs, int rowNum) ->{
-                    Author author = new Author();
-                    author.setId(rs.getInt("id"));
-                    author.setFirstName(rs.getString("first_name"));
-                    author.setLastName(rs.getString("last_name"));
-                    return author;
-                });
+        List<Author> authors = authorRepository.findAll();
         return authors.stream().collect(Collectors.groupingBy((Author o) -> {return o.getLastName().substring(0,1);}));
     }
 }
